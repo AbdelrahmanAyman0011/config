be4327fa46247d3acc8a27a5ce425e4cd11a2f17
AbdelrahmanAyman0011 <abdelrahmanayman0011@gmail.com>
2026-01-08 05:21:00 +0200

    update animation

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
+-- NEOVIDE CURSOR (Neovide-Cursor)
 if vim.g.neovide then
-  vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_cursor_animation_length = 0.13
+  vim.g.neovide_cursor_trail_size = 0.8
+  vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_cursor_vfx_mode = "railgun"
 end
 
 require "conf"
