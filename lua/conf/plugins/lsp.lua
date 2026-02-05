7c246428b3eb8f567e95c8e528ff86a45fe4e0ec
Abdelrahamn Ayaman <115227760+AbdelrahmanAyman0011@users.noreply.github.com>
2026-02-05 19:11:48 +0200

    Add Java, Docker, SQL, and Bash LSP autocomplete support

--- lua/conf/plugins/lsp.lua
+++ lua/conf/plugins/lsp.lua
@@ -8,7 +8,8 @@ return {
   },
 
   config = function()
-  vim.api.nvim_create_autocmd('LspAttach', {
+    -- Keybindings when LSP attaches
+    vim.api.nvim_create_autocmd('LspAttach', {
       desc = 'LSP actions',
       callback = function(event)
         local opts = {buffer = event.buf}
@@ -26,142 +27,129 @@ return {
       end,
     })
 
-
     -- Mason setup
-    require("mason").setup({
-      ensure_installed = {
-      },
-    })
+    require("mason").setup({})
 
-    -- Mason-lspconfig setup
+    -- Get capabilities for autocomplete
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+    -- Mason-lspconfig setup with automatic installation
     require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",              -- Lua
+        "clangd",              -- C/C++
+        "pyright",             -- Python
+        "gopls",               -- Go
+        "jdtls",               -- Java
+        "dockerls",            -- Dockerfile
+        "docker_compose_language_service", -- Docker Compose
+        "sqlls",               -- SQL
+        "bashls",              -- Bash
+      },
+      automatic_installation = true,
       handlers = {
-        -- Default handler for automatically configuring installed servers
+        -- Default handler - applies to all servers
         function(server_name)
-          require("lspconfig")[server_name].setup({}) end,
-
-        -- You can add custom handlers for specific servers here
-        -- For example:
-        -- tsserver = function(_, opts)
-        --   require("lspconfig").tsserver.setup(opts)
-        -- end,
+          require("lspconfig")[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+        
+        -- Custom handler for lua_ls
+        ["lua_ls"] = function()
+          require("lspconfig").lua_ls.setup({
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" },
+                },
+                workspace = {
+                  library = vim.api.nvim_get_runtime_file("", true),
+                  checkThirdParty = false,
+                },
+                telemetry = {
+                  enable = false,
+                },
+              },
+            },
+          })
+        end,
       },
     })
--- =========================
--- Neovim 0.11+ LSP CONFIG
--- =========================
 
-vim.lsp.config("lua_ls", {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-    },
-  },
-})
-
-vim.lsp.config("clangd", {})
-vim.lsp.config("pyright", {})
-vim.lsp.config("gopls",{})
-vim.lsp.config("jdtls",{}) -- java
--- Enable servers (NEW API)
-vim.lsp.enable({
-  "lua_ls",
-  "clangd",
-  "pyright",
-  "gopls",
-  "jdtls",
-})
-
+    -- nvim-cmp setup
     local cmp = require("cmp")
 
-    --   פּ ﯟ   some other good icons
     local kind_icons = {
-      Text = "",
+      Text = "",
       Method = "m",
-      Function = "",
-      Constructor = "",
-      Field = "",
-      Variable = "",
-      Class = "",
-      Interface = "",
-      Module = "",
-      Property = "",
-      Unit = "",
-      Value = "",
-      Enum = "",
-      Keyword = "",
-      Snippet = "",
-      Color = "",
-      File = "",
-      Reference = "",
-      Folder = "",
-      EnumMember = "",
-      Constant = "",
-      Struct = "",
-      Event = "",
-      Operator = "",
-      TypeParameter = "",
+      Function = "",
+      Constructor = "",
+      Field = "",
+      Variable = "",
+      Class = "",
+      Interface = "",
+      Module = "",
+      Property = "",
+      Unit = "",
+      Value = "",
+      Enum = "",
+      Keyword = "",
+      Snippet = "",
+      Color = "",
+      File = "",
+      Reference = "",
+      Folder = "",
+      EnumMember = "",
+      Constant = "",
+      Struct = "",
+      Event = "",
+      Operator = "",
+      TypeParameter = "",
     }
-    -- find more here: https://www.nerdfonts.com/cheat-sheet
 
     cmp.setup({
       sources = {
         {name = "nvim_lsp"},
       },
 
-     mapping = {
-       ["<C-k>"] = cmp.mapping.select_prev_item(),
-       ["<C-j>"] = cmp.mapping.select_next_item(),
-       ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-       ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-       ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-       ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-       ["<C-e>"] = cmp.mapping {
-         i = cmp.mapping.abort(),
-         c = cmp.mapping.close(),
-       },
-       -- Accept currently selected item. If none selected, `select` first item.
-       -- Set `select` to `false` to only confirm explicitly selected items.
-       ["<CR>"] = cmp.mapping.confirm { select = false },
-       ["<Tab>"] = cmp.mapping(function(fallback)
-         if cmp.visible() then
-           cmp.select_next_item()
-         else
-           fallback()
-         end
-       end, {
-         "i",
-         "s",
-       }),
-       ["<S-Tab>"] = cmp.mapping(function(fallback)
-         if cmp.visible() then
-           cmp.select_prev_item()
-         else
-           fallback()
-         end
-       end, {
-         "i",
-         "s",
-       }),
-     },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
 
-    formatting = {
-      fields = { "kind", "abbr", "menu" },
-      format = function(entry, vim_item)
-        -- Kind icons
-        vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-        -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-        vim_item.menu = ({
-          nvim_lsp = "[LSP]",
-          --nvim_lsp_signature_help = "[LSP-Signature]",
-          buffer = "[Buffer]",
-          path = "[Path]",
-        })[entry.source.name]
-        return vim_item
-      end,
-    }
-  })
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+          vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+          vim_item.menu = ({
+            nvim_lsp = "[LSP]",
+            buffer = "[Buffer]",
+            path = "[Path]",
+          })[entry.source.name]
+          return vim_item
+        end,
+      }
+    })
   end
 }
