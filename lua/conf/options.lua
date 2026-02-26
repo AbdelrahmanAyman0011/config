699484b056645c35a2396b94245119a3f5fe6c49
AbdelrahmanAyman0011 <abdelrahmanayman0011@gmail.com>
2026-02-26 04:54:17 +0200

    change font

--- lua/conf/options.lua
+++ lua/conf/options.lua
@@ -34,7 +34,7 @@ local options = {
   wrap = false,
   scrolloff = 4,
   sidescrolloff = 4,
-  guifont = "JetBrainsMono_Nerd_Font:h12", -- Changed from h11 to h9 (smaller)
+  guifont = "ComicShannsMono_Nerd_Font_Mono:h16",
   --guifont = "Cascadia_Mono:h10",
 }
 
