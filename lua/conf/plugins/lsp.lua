1c706f66fbe296a7d2d1129453e4094832476a51
AbdelrahmanAyman0011 <abdelrahmanayman0011@gmail.com>
2026-04-15 01:09:00 +0200

    update error alerts

--- lua/conf/plugins/lsp.lua
+++ lua/conf/plugins/lsp.lua
@@ -27,6 +27,46 @@ return {
       end,
     })
 
+    -- Configure Diagnostics (Errors/Warnings)
+    local signs = {
+      { name = "DiagnosticSignError", text = "" },
+      { name = "DiagnosticSignWarn", text = "" },
+      { name = "DiagnosticSignHint", text = "" },
+      { name = "DiagnosticSignInfo", text = "" },
+    }
+
+    for _, sign in ipairs(signs) do
+      vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+    end
+
+    vim.diagnostic.config({
+      virtual_text = {
+        spacing = 4,
+        prefix = "●",
+      },
+      signs = {
+        active = signs,
+      },
+      update_in_insert = true,
+      underline = true,
+      severity_sort = true,
+      float = {
+        focusable = true,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    })
+
+    -- Show diagnostic popup on cursor hold
+    vim.api.nvim_create_autocmd("CursorHold", {
+      callback = function()
+        vim.diagnostic.open_float(nil, { focusable = false })
+      end,
+    })
+
     -- Mason setup
     require("mason").setup({})
 
